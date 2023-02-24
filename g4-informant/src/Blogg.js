9bc52f4cbee4224ac2971896a2965728b8b836d2
karlo <karloscarfs@hotmail.com>
2023-02-24 15:53:20 +0100

    more blogg

--- g4-informant/src/Blogg.js
+++ g4-informant/src/Blogg.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { useState } from "react";
 
-function Blogg(){
-    return <div>This is blogg</div>;
+const Blogg = () => {
+  
+  const [blogs, setBlogs] = useState([    
+{ title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+{ title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+{ title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+]);
+
+  return (
+    <div className="blogg">
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+        <h2>{ blog.title }</h2>
+        <p>Written by { blog.author }</p>
+        </div>
+      ))}
+    </div>
+  );
 }
-
+ 
 export default Blogg;
