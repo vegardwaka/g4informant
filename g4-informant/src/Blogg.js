225a0dea3b30d7bcef653f17a63576a723d714ee
karlo <karloscarfs@hotmail.com>
2023-02-28 15:40:48 +0100

    blog list sort

--- g4-informant/src/Blogg.js
+++ g4-informant/src/Blogg.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { useState } from "react";
+import BlogList from "./BlogList";
 
-const Blogg = () => {
-  
+const Blogg = () => { 
   const [blogs, setBlogs] = useState([    
 { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
 { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
@@ -11,12 +11,8 @@ const Blogg = () => {
 
   return (
     <div className="blogg">
-      {blogs.map((blog) => (
-        <div className="blog-preview" key={blog.id}>
-        <h2>{ blog.title }</h2>
-        <p>Written by { blog.author }</p>
-        </div>
-      ))}
+      <BlogList blogs={blogs} title="All Bloggs" />
+      <BlogList blogs={blogs.filter((blog)=> blog.author === "mario")} title="Marios bloggs" />
     </div>
   );
 }
