b30734607c944aaaccc07ead57eb16e9bad16a6e
Karl Oscar FS <240839@usn.no>
2023-03-14 15:16:56 +0100

    forespørsel
    Co-authored-by: vegardwaka <vegardwaka@users.noreply.github.com>
    Co-authored-by: Vebteo <Vebteo@users.noreply.github.com>
    Co-authored-by: SigveDankel <SigveDankel@users.noreply.github.com>

--- g4-informant/src/components/Foresporsel.js
+++ g4-informant/src/components/Foresporsel.js
@@ -1,17 +1,61 @@
-import { useFetch } from "react-fetch-hook"
+import { useState } from "react";
+import { useNavigate } from 'react-router-dom';
+
 export default function Foresporsel() {
-    //const {isLoading, data} = useFetch("https://g4informant.com/api.php/records/bruker/1")
+    const [title, setTitle] = useState("");
+    const [category, setCategory] = useState('');
+    const [message, setMessage] = useState('');
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const apiReq = {title, category, message};
+        console.log(e.target.title.value)
+        console.log(e.target.category.value)
+        console.log(e.target.message.value)
+
+        fetch("https://g4informant.com/api.php/records/api_foresporsel", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({"API_id": 0, "tittel": title, "fk_bruker_id": 2, "fritekst": message, "kategori": category}),
+            })  
+            .then((response) => response.json())
+            .then((apiReq) => {
+                console.log("Success:", apiReq);
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+            });
+    }
+
+    
     return (    
         
-        
         <div className="foresporsel--div">
-            <form className="foresporsel--form">
+            <form className="foresporsel--form" onSubmit={handleSubmit}>
                 <h1 className="foresporsel--tittel">API forespørsel</h1>
-                <label for="foresporsel-label">Tittel</label><br/>
-                <input type="text" className="foresporsel-label"/><br/>
-                <label for="cars" className="foresporsel-label">Velg en kategori:</label>
+                <label htmlFor="foresporsel-label">Tittel</label><br/>
+                <input 
+                    required
+                    value={title} 
+                    onChange={(e) => setTitle(e.target.value)}
+                    type="text"
+                    name="title" 
+                    className="foresporsel-label"
+                />
+                <br/>
+                <label htmlFor="cars" className="foresporsel-label">Velg en kategori:</label>
 
-                <select id="cars" name="cars" className="foresporsel--liste">
+                <select 
+                    id="cars"  
+                    required
+                    value={category} 
+                    onChange={(e) => setCategory(e.target.value)}
+                    name="category"
+                    className="foresporsel--liste"
+                >
                     <option value="Velg">Velg</option>
                     <option value="Nyheter">Nyheter</option>
                     <option value="Vær">Vær</option>
@@ -19,9 +63,18 @@ export default function Foresporsel() {
                     <option value="Jobb">Jobb</option>
                 </select><br/>
 
-                <label for="test">Fritekst</label><br/>
-                <textarea name="message" rows="10" cols="30" className="foresporsel--fritekst"></textarea><br/>
-                <button className="foresporsel--button">SEND INN</button>
+                <label htmlFor="test">Fritekst</label><br/>
+                <textarea  
+                    required
+                    value={message}  
+                    onChange={(e) => setMessage(e.target.value)}
+                    name="message"
+                    rows="10" 
+                    cols="30" 
+                    className="foresporsel--fritekst"
+                >
+                    </textarea><br/>
+                <button type="submit" className="foresporsel--button">SEND INN</button>
             </form>
         </div>
     )
